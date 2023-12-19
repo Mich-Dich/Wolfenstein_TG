c7ff6ea5a570ed74ea1e59e557544f99368b53e9
Mich <mally.dominik@gmx.de>
2023-12-19 22:22:55 +0100

    bash script update

--- AutoGenerateProject.bash
+++ AutoGenerateProject.bash
@@ -1,16 +1,44 @@
 #!/bin/bash
 
+# Default configuration
+configuration="Release"
+
+# Process command-line arguments
+while getopts ":c:" opt; do
+  case $opt in
+    c)
+      configuration=$OPTARG
+      echo "       TARGET: $configuration" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 directory="build"
 
 # Check if the directory exists
-if [ -d "$directory" ]; then
+if [ ! -d "$directory" ]; then
     mkdir -p "$directory" || exit 1
 fi
 
-# Rest of your script...
-premake4 gmake --zo=./build
-
+# Generate Makefiles using premake4 for the specified configuration
+premake4 gmake --config="$configuration"
 cd build
 make
 
-exit
+# Check if the make command was successful
+if [ $? -eq 0 ]; then
+    # Run the compiled binary for the specified configuration
+    ./bin_linux/Wolfenstein_TG  # Adjust the path based on your project structure
+else
+    echo "Build failed. Execution aborted."
+    exit 1
+fi
+
--- premake4.lua
+++ premake4.lua
@@ -10,7 +10,7 @@ location "build" -- location for the generated project files
 project "Wolfenstein_TG"
     kind "ConsoleApp"
     language "C"
-    targetdir ("build/bin/" .. os.get("cfg") .. "/") -- output directory for the binaries
-    objdir ("build/bin-int/" .. os.get("cfg") .. "/")  -- output directory for intermediate files
+    targetdir ("build/bin_" .. os.get("cfg") .. "/") -- output directory for the binaries
+    objdir ("build/bin-int_" .. os.get("cfg") .. "/")  -- output directory for intermediate files
 
     files { "src/*.c" } -- Specify the correct path to the source files
